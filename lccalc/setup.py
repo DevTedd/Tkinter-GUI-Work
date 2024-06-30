1717da181975e25d7cb4739ca1d460a836e6d5b3
Kimuhu <aueskinj@gmail.com>
2024-06-30 11:00:00 +0300

    fixing virtual environment...missing modules error

--- lccalc/setup.py
+++ lccalc/setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 from Cython.Build import cythonize
 
 setup(
-    ext_modules=cythonize("main.py", compiler_directives={'language_level': "3"}),
+    ext_modules=cythonize("script.pyx", compiler_directives={'language_level': "3"}),
 )
